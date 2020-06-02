710fb51279a0f83d52fd66f2d3f6a9a4c87bfd22
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2020-06-02 10:34:14 +0300

    Adds FilePartReader and FileWordAnalyzer

---
+++ src/main/java/com/codecool/FilePartReader.java
@@ -0,0 +1,23 @@
+package com.codecool;
+
+public class FilePartReader {
+    private String filePath;
+    private int fromLine;
+    private int toLine;
+
+    public FilePartReader() {
+    }
+
+    public void setup (){
+
+    }
+
+    public String read (){
+
+    }
+
+    public String readLines (int fromLine, int toLine){
+
+    }
+
+}
---
+++ src/main/java/com/codecool/FileWordAnalyzer.java
@@ -0,0 +1,19 @@
+package com.codecool;
+
+import java.util.List;
+
+public class FileWordAnalyzer {
+    private FilePartReader filePartReader;
+
+    public List getWordsOrderedAlphabetically (){
+
+    }
+
+    public List getWordsContainingSubstring (String subString ){
+
+    }
+
+    public List getStringsWhichPalindromes (){
+
+    }
+}
