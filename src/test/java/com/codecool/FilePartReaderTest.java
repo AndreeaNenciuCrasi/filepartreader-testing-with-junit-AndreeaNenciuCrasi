c711fad960ccfcff8a97a37aa553ff75b8e17149
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2020-06-02 15:50:44 +0300

    Adds test classes

---
+++ src/test/java/com/codecool/FilePartReaderTest.java
@@ -0,0 +1,21 @@
+package com.codecool;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilePartReaderTest {
+
+    @org.junit.jupiter.api.Test
+    void setup() {
+        fail("This test is not implemented");
+    }
+
+    @org.junit.jupiter.api.Test
+    void read() {
+        fail("This test is not implemented");
+    }
+
+    @org.junit.jupiter.api.Test
+    void readLines() {
+        fail("This test is not implemented");
+    }
+}
---
+++ src/test/java/com/codecool/FileWordAnalyzerTest.java
@@ -0,0 +1,23 @@
+package com.codecool;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileWordAnalyzerTest {
+
+    @Test
+    void getWordsOrderedAlphabetically() {
+        fail("This test is not implemented");
+    }
+
+    @Test
+    void getWordsContainingSubstring() {
+        fail("This test is not implemented");
+    }
+
+    @Test
+    void getStringsWhichPalindromes() {
+        fail("This test is not implemented");
+    }
+}
