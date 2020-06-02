8fd0b2ca39c04f622940b636bcbc8858ae54f79c
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2020-06-02 17:19:03 +0300

    Adds tests to FileWordAnalyzerTest file

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -5,26 +5,26 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         FilePartReader textFromFile = new FilePartReader();
-        textFromFile.setup(textFromFile.getFilePath(),2, 5);
+        textFromFile.setup(textFromFile.getFilePath(),1, 10);
         String data = textFromFile.read();
-        System.out.println(data);
+//        System.out.println(data);
 
         String readLines = textFromFile.readLines();
 //        System.out.println(readLines);
-//        FileWordAnalyzer analyzer = new FileWordAnalyzer(textFromFile);
-////      List<String> str1 = analyzer.getWordsOrderedAlphabetically();
-////        for(String i: str1) {
-////            System.out.println(i);
-////        }
+        FileWordAnalyzer analyzer = new FileWordAnalyzer(textFromFile);
+//      List<String> str1 = analyzer.getWordsOrderedAlphabetically();
+//        for(String i: str1) {
+//            System.out.println(i);
+//        }
 //        List<String> str2 = analyzer.getWordsContainingSubstring("fi");
 //        for(String i: str2) {
 //            System.out.println(i);
 //        }
 //
-//        List<String> str3 = analyzer.getStringsWhichPalindromes();
-//        for(String i: str3) {
-//            System.out.println(i);
-//        }
+        List<String> str3 = analyzer.getStringsWhichPalindromes();
+        for(String i: str3) {
+            System.out.println(i);
+        }
     }
 
 }
--- src/test/java/com/codecool/FileWordAnalyzerTest.java
+++ src/test/java/com/codecool/FileWordAnalyzerTest.java
@@ -2,22 +2,43 @@ package com.codecool;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FileWordAnalyzerTest {
 
     @Test
     void getWordsOrderedAlphabetically() {
-        fail("This test is not implemented");
+        FilePartReader fileReader = new FilePartReader();
+        fileReader.setup("src/main/resources/text_file.txt",2,5);
+        FileWordAnalyzer analyzer = new FileWordAnalyzer(fileReader);
+        List<String> result= analyzer.getWordsOrderedAlphabetically();
+        List<String> expected = Arrays.asList("a", "a","a", "a", "amount", "by", "contents", "create", "different", "do", "file",
+                    "file", "file", "from", "from", "how", "how", "how", "i", "in", "java", "java", "line", "lines", "n", "new", "of",
+                    "of", "of", "read", "reading", "solos", "split", "string", "string", "text", "the", "to", "to", "ways");
+        assertArrayEquals(expected.toArray(),result.toArray());
     }
 
     @Test
     void getWordsContainingSubstring() {
-        fail("This test is not implemented");
+        FilePartReader fileReader = new FilePartReader();
+        fileReader.setup("src/main/resources/text_file.txt",1,10);
+        FileWordAnalyzer analyzer = new FileWordAnalyzer(fileReader);
+        List<String> result = analyzer.getWordsContainingSubstring("fi");
+        List<String> expected = Arrays.asList("files", "file", "file", "file", "files", "file", "file", "file");
+        assertArrayEquals(expected.toArray(),result.toArray());
+
     }
 
     @Test
     void getStringsWhichPalindromes() {
-        fail("This test is not implemented");
+        FilePartReader fileReader = new FilePartReader();
+        fileReader.setup("src/main/resources/text_file.txt",1,10);
+        FileWordAnalyzer analyzer = new FileWordAnalyzer(fileReader);
+        List<String> result = analyzer.getStringsWhichPalindromes();
+        List<String> expected = Arrays.asList("solos", "sagas", "madam", "racecar");
+        assertArrayEquals(expected.toArray(),result.toArray());
     }
 }
